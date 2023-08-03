cce262bd252364f60aa90a958b9736878ac98515
Matt <matt.huang@thekono.com>
2023-08-04 03:13:13 +0800

    feat(routes): Add clock_in, clock_out routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :client_info, only: :index
+
   resources :users do
     post :follow, on: :member
     post :unfollow, on: :member
+
+    post 'clock_in', to: 'clocks#create'
+    put 'clock_out', to: 'clocks#update'
   end
 end
