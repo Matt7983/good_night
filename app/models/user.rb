e294d5b7075477d2411034afff277375dc7f6208
Matt <matt.huang@thekono.com>
2023-08-03 19:08:53 +0800

    feat(model): Add relationship between users and followers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :followers, class_name: "Follower", foreign_key: "followee_id", primary_key: "id", dependent: :destroy
+
+  def follower_ids
+    followers.pluck(:follower_id)
+  end
+
+  def followed_users
+    User.where(id: follower_ids)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#follower_ids' do
+    let!(:followee) { FactoryBot.create(:user) }
+    let!(:followers) { FactoryBot.create_list(:user, 3) }
+
+    before do
+      followers.each do |follower|
+        Follower.create(followee_id: followee.id, follower_id: follower.id)
+      end
+    end
+
+    it 'returns the ids of the followers' do
+      expect(followee.follower_ids).to match_array(followers.map(&:id))
+    end
+  end
+
+  describe '#followed_users' do
+    let!(:followee) { FactoryBot.create(:user) }
+    let!(:followers) { FactoryBot.create_list(:user, 3) }
+
+    before do
+      followers.each do |follower|
+        Follower.create(followee_id: followee.id, follower_id: follower.id)
+      end
+    end
+
+    it 'returns the users who follow the user' do
+      expect(followee.followed_users).to match_array(followers)
+    end
+  end
 end
