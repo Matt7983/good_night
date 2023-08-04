791dc1941314906a734ff5e1be198e4ccfa4f97b
Matt <matt.huang@thekono.com>
2023-08-05 02:29:08 +0800

    feat(seeds): Update seeds.rb for User/Follower/Clock models.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+followee = User.create!(name: Faker::Name.name)
+follower1 = User.create!(name: Faker::Name.name)
+follower2 = User.create!(name: Faker::Name.name)
+Follower.create!(followee_id: followee.id, follower_id: follower1.id)
+Follower.create!(followee_id: followee.id, follower_id: follower2.id)
+Clock.create!(user_id: follower1.id, clock_in: 8.days.before, clock_out: 7.days.before, duration: 1.day)
+Clock.create!(user_id: follower1.id, clock_in: 6.days.before, clock_out: 4.days.before, duration: 2.days)
+Clock.create!(user_id: follower1.id, clock_in: 10.hours.before, clock_out: 8.hours.before, duration: 2.hours)
+Clock.create!(user_id: follower2.id, clock_in: 4.days.before, clock_out: 3.days.before, duration: 1.day)
+Clock.create!(user_id: follower2.id, clock_in: 48.hours.before, clock_out: 12.hours.before, duration: 36.hours)
